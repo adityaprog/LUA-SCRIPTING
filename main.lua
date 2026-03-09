1dc62e11cd1a9243c6aaa3917d6e6cec5a649512
ADiTyA raj <113365136+adityaprog@users.noreply.github.com>
2026-03-09 18:53:48 +0000

    File names are case sensitive on linux terminal. Meaning 'main.LUA' is not same as 'main.lua'. Fixed the error

--- main.lua
+++ main.lua
@@ -1,1 +1,3 @@
-'Hello! World'
+-- This is my first time using LUA Scripting language.
+-- Double hyphen means that this line is a comment as will not be excuted (or say, ignored by the interpreter.)
+this will show err
