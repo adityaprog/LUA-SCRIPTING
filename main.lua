907043953a339d137b1eb5223dc070b027ed3f48
ADiTyA raj <113365136+adityaprog@users.noreply.github.com>
2026-03-09 19:06:24 +0000

    First program with excessive commments

--- main.lua
+++ main.lua
@@ -1,3 +1,27 @@
+--[[
+To run this file, install lua of any verion (I am not sure!)
+This file is made on lua 5.4.6
+
+To execute,
+Type:
+    lua <filename>
+Example:
+    lua main.lua
+
+Be sure to type the exact file name as it is case sensitive.
+]]
+
+
+
 -- This is my first time using LUA Scripting language.
--- Double hyphen means that this line is a comment as will not be excuted (or say, ignored by the interpreter.)
-this will show err
+-- Double hyphen means that this line is a comment as will not be executed (or say, ignored by the interpreter.)
+
+--[[
+NOW, 
+this is a multi line comment.
+
+With two Hyphen and two square brackets without space.
+
+]]
+
+print("Hello Viewers!") --print is used to get results on terminal
