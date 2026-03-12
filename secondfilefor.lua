5e0a8c38adb88bbc12fab883dc0cdd1dbcca14d8
ADiTyA raj <113365136+adityaprog@users.noreply.github.com>
2026-03-12 14:11:11 +0000

    Comments and variables

--- secondfilefor.lua
+++ secondfilefor.lua
@@ -3,6 +3,8 @@
 'hello', 'world'  --> hello world
 'hello'..'world' --> helloworld
 
+..  --> changes number into string automatically
+
 'hello', 2 --> hello 2
 'hello'..2 --> hello2
 
@@ -11,4 +13,14 @@ For text/strings, use quotes --> ' ', " "
 
 ]]
 
-print("1 + 2 =", 1+2)
+-- print("1 + 2 =", 1+2)
+
+local n_line = string.char(10)
+
+Name = 'John Deo'
+Age = '99'
+Place = 'Avengers Tower'
+
+local message = 'My name is %s, I am %d years old.' .. n_line .. 'I live in %s'
+
+print(string.format(message, Name, Age, Place))
